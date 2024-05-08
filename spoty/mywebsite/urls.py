927868717abcaed625accd5caebe8b0b77bf63df
moonlight-wtbh <nuray.altynbekovas@gmail.com>
2024-05-08 20:43:43 +0500

    Added registration page

--- spoty/mywebsite/urls.py
+++ spoty/mywebsite/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from register import views as vreg
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("register/", vreg.register, name="register"),
     path("todos/", views.todos, name="todos")
 ]
--- spoty/spoty/settings.py
+++ spoty/spoty/settings.py
@@ -35,9 +35,12 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    "crispy_forms",
+    "crispy_bootstrap5",
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "mywebsite"
+    "mywebsite.apps.MywebsiteConfig",
+    "register.apps.RegisterConfig"
 ]
 
 MIDDLEWARE = [
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK="bootstrap5"
